1903b9e49e4ff87ad573058822acd62451ef796c
AlexandreJareck <alexandrejareck@gmail.com>
2023-09-12 20:39:03 -0300

    feat: enriquencendo entidade categoria

--- src/NerdStore.Catalogo.Domain/Categoria.cs
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -13,9 +13,23 @@ namespace NerdStore.Catalogo.Domain
         protected Categoria()
         { }
 
+        public Categoria(string nome, int codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+
+            Validar();
+        }
+
         public override string ToString()
         {
             return $"{Nome} - {Codigo}";
+        }
+
+        public void Validar()
+        {
+            Validacoes.ValidarSeVazio(Nome, "O campo Nome da categoria não pode estar vazio");
+            Validacoes.ValidarSeIgual(Codigo, 0, "O campo Codigo não pode ser 0");
         }
     }
 }
