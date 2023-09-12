2d1b6153732202fd48649fcf5ceb85b20d4a45df
AlexandreJareck <alexandrejareck@gmail.com>
2023-09-12 20:37:02 -0300

    feat: modelando entidade de dominio -> categoria

---
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using NerdStore.Core.DomainObject;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+        public int Codigo { get; private set; }
+
+        // EF Relation
+        public ICollection<Produto> Produtos { get; set; }
+
+        protected Categoria()
+        { }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}
