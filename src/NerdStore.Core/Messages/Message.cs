16cac4ee5245e711349628999d0ed5197f9d1677
AlexandreJareck <alexandrejareck@gmail.com>
2023-09-13 20:45:59 -0300

    feat: adiciona class Message para trabalhar futuramente com commands, e events

---
+++ src/NerdStore.Core/Messages/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace NerdStore.Core.Messages
+{
+    public abstract class Message
+    {
+        public string Messagetype { get; protected set; }
+        public Guid AggregateId { get; set; }
+
+        protected Message()
+        {
+            Messagetype = GetType().Name;
+        }
+    }
+}
