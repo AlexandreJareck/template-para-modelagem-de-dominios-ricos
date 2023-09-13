911bd6713f22feadfe17b3e47834b22d3e524c4d
AlexandreJareck <alexandrejareck@gmail.com>
2023-09-13 20:48:41 -0300

    feat: adiciona class base para events -> DomainEvent

---
+++ src/NerdStore.Core/DomainObject/DomainEvent.cs
@@ -0,0 +1,12 @@
+﻿using NerdStore.Core.Messages;
+
+namespace NerdStore.Core.DomainObject
+{
+    public class DomainEvent : Event
+    {
+        public DomainEvent(Guid aggregateId)
+        {
+            AggregateId = aggregateId;
+        }
+    }
+}
