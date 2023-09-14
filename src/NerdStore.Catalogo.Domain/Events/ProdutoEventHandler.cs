79abe7c6ab0ab2305b304fbea4517a13fdc12c55
AlexandreJareck <alexandrejareck@gmail.com>
2023-09-13 21:21:04 -0300

    feat: adiciona regra de negócio no estoque service. Quando a quantidade o produto estiver abaixo de 10 no estoque.

---
+++ src/NerdStore.Catalogo.Domain/Events/ProdutoEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+
+namespace NerdStore.Catalogo.Domain.Events
+{
+    public class ProdutoEventHandler : INotificationHandler<ProdutoAbaixoEstoqueEvent>
+    {
+        private readonly IProdutoRepository _produtoRepository;
+
+        public ProdutoEventHandler(IProdutoRepository produtoRepository)
+        {
+            _produtoRepository = produtoRepository;
+        }
+
+        public async Task Handle(ProdutoAbaixoEstoqueEvent mensagem, CancellationToken cancellationToken)
+        {
+            var produto = _produtoRepository.ObterPorId(mensagem.AggregateId);
+
+            // Aqui é somente um exemplo do pode agora com essa regra de negócio.
+            // 1. Você pode enviar email
+            // 2. Você pode enviar um whats app
+            // Várias possibilidades...
+
+            // Essa foi uma implementação de exemplo, no caso poderia existir varias regras de negocio, e essas regras seguirem essas estrutura.
+
+            return;
+        }
+    }
+}
