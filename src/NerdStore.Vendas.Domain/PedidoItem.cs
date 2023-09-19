4c7557c727f9f78f30a1124f1a97dff6d6804c68
AlexandreJareck <alexandrejareck@gmail.com>
2023-09-18 22:00:05 -0300

    refactor: adiciona nullable na class pedido item

--- src/NerdStore.Vendas.Domain/PedidoItem.cs
+++ src/NerdStore.Vendas.Domain/PedidoItem.cs
@@ -6,11 +6,11 @@ namespace NerdStore.Vendas.Domain
     {
         public Guid PedidoId { get; private set; }
         public Guid ProdutoId { get; private set; }
-        public string ProdutoNome { get; private set; }
+        public string? ProdutoNome { get; private set; }
         public int Quantidade { get; private set; }
         public decimal ValorUnitario { get; private set; }
 
-        public Pedido Pedido { get; set; }
+        public Pedido? Pedido { get; set; }
 
         public PedidoItem(Guid produtoId, string produtoNome, int quantidade, decimal valorUnitario)
         {
