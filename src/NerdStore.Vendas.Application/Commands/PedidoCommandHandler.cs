0eea43cbc431c98efcb4a28f0bf6424129bf7ff2
AlexandreJareck <alexandrejareck@gmail.com>
2023-09-18 21:55:34 -0300

    feat: adiciona class Pedido Command Handler, para manipular os comandos do pedido (métodos não implementados ainda)

---
+++ src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace NerdStore.Vendas.Application.Commands
+{
+    public class PedidoCommandHandler :
+        IRequestHandler<AdicionarItemPedidoCommand, bool>
+
+    {
+        public Task<bool> Handle(AdicionarItemPedidoCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
