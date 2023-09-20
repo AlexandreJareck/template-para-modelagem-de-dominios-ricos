bb2fe92e3cf4dc0ffc16f5a71295bc4f97e5ba08
AlexandreJareck <alexandrejareck@gmail.com>
2023-09-19 22:23:24 -0300

    feat: implementa método para manipular evento de adicionar item ao carrinho

--- src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
+++ src/NerdStore.Vendas.Application/Commands/PedidoCommandHandler.cs
@@ -1,14 +1,65 @@
 ﻿using MediatR;
+using NerdStore.Core.Messages;
+using NerdStore.Vendas.Application.Events;
+using NerdStore.Vendas.Domain;
 
 namespace NerdStore.Vendas.Application.Commands
 {
-    public class PedidoCommandHandler :
-        IRequestHandler<AdicionarItemPedidoCommand, bool>
+    public class PedidoCommandHandler : IRequestHandler<AdicionarItemPedidoCommand, bool>
 
     {
-        public Task<bool> Handle(AdicionarItemPedidoCommand request, CancellationToken cancellationToken)
+        private readonly IPedidoRepository _pedidoRepository;
+
+        public PedidoCommandHandler(IPedidoRepository pedidoRepository)
         {
-            throw new NotImplementedException();
+            _pedidoRepository = pedidoRepository;
+        }
+
+        public async Task<bool> Handle(AdicionarItemPedidoCommand message, CancellationToken cancellationToken)
+        {
+            if (!ValidarComando(message)) return false;
+
+            var pedido = await _pedidoRepository.ObterPedidoRascunhoPorClienteId(message.ClienteId);
+            var pedidoItem = new PedidoItem(message.ProdutoId, message.Nome, message.Quantidade, message.ValorUnitario);
+
+            if (pedido is null)
+            {
+                pedido = Pedido.PedidoFactory.NovoPedidoRascunho(message.ClienteId);
+                pedido.AdicionarItem(pedidoItem);
+
+                _pedidoRepository.Adicionar(pedido);
+            }
+            else
+            {
+                var pedidoItemExistente = pedido.PedidoItemExistente(pedidoItem);
+                pedido.AdicionarItem(pedidoItem);
+
+                if (pedidoItemExistente)
+                {
+                    _pedidoRepository.AtualizarItem(pedido.PedidoItems.FirstOrDefault(p => p.ProdutoId == pedidoItem.ProdutoId));
+                }
+                else
+                {
+                    _pedidoRepository.AdicionarItem(pedidoItem);
+                }
+
+                pedido.AdicionarEvento(new PedidoAtualizadoEvent(pedido.ClienteId, pedido.Id, pedido.ValorTotal));
+            }
+
+            pedido.AdicionarEvento(new PedidoItemAdicionadoEvent(pedido.ClienteId, pedido.Id, message.ProdutoId, message.Nome, message.ValorUnitario, message.Quantidade));
+            return await _pedidoRepository.UnitOfWork.Commit();
+        }
+
+        private bool ValidarComando(Command message)
+        {
+            if (message.EhValido()) return true;
+
+            foreach (var item in message.ValidationResult.Errors)
+            {
+                // lançar evento
+            }
+
+            return false;
         }
     }
 }
